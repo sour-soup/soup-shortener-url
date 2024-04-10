567c834205abcecd779044fedadff1fab8aabef0
sour-soup <kiziyepi8@gmail,com>
2024-04-11 02:16:44 +0400

    ы2

---
+++ soup-shortener-url/src/main/java/org/example/repository/impl/LinkRepositoryImpl.java
@@ -0,0 +1,35 @@
+package org.example.repository.impl;
+
+import org.example.database.EmulationDatabase;
+import org.example.repository.LinkRepository;
+import org.example.repository.dao.IdDao;
+import org.example.repository.dao.LinkDao;
+
+public class LinkRepositoryImpl implements LinkRepository {
+    @Override
+    public void addLink(IdDao idDao, LinkDao linkDao) {
+        EmulationDatabase.getInstance().addLink(idDao.id(), linkDao.link());
+    }
+
+    @Override
+    public LinkDao getLink(IdDao idDao) {
+        String link = EmulationDatabase.getInstance().getLink(idDao.id());
+        return new LinkDao(link);
+    }
+
+    @Override
+    public IdDao getId(LinkDao linkDao) {
+        Long id = EmulationDatabase.getInstance().getId(linkDao.link());
+        return new IdDao(id);
+    }
+
+    @Override
+    public Boolean checkLink(LinkDao linkDao) {
+        return EmulationDatabase.getInstance().checkLink(linkDao.link());
+    }
+
+    @Override
+    public Boolean checkId(IdDao idDao) {
+        return EmulationDatabase.getInstance().checkId(idDao.id());
+    }
+}
---
+++ soup-shortener-url/src/main/java/org/example/service/impl/LinkServiceImpl.java
@@ -0,0 +1,46 @@
+package org.example.service.impl;
+
+import org.example.repository.LinkRepository;
+import org.example.repository.dao.IdDao;
+import org.example.repository.dao.LinkDao;
+import org.example.service.LinkService;
+import org.example.service.model.LongLink;
+import org.example.service.model.ShortLink;
+import org.example.utils.BaseConversion;
+import org.example.utils.BaseConversionException;
+
+public class LinkServiceImpl implements LinkService {
+    private static final Long MIN_ID = 1L;
+    private static final Long MAX_ID = 56800235583L;
+    private final LinkRepository linkRepository;
+
+    public LinkServiceImpl(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    @Override
+    public ShortLink addLink(LongLink longLink) throws BaseConversionException {
+        LinkDao linkDao = new LinkDao(longLink.link());
+        if (linkRepository.checkLink(linkDao)) {
+            IdDao idDao = linkRepository.getId(linkDao);
+            String link = BaseConversion.toBase(idDao.id());
+            return new ShortLink(link);
+        }
+        Long id = MIN_ID + (long) (Math.random() * (MAX_ID - MIN_ID));
+        while (linkRepository.checkId(new IdDao(id)))
+            id = MIN_ID + (long) (Math.random() * (MAX_ID - MIN_ID));
+        String link = BaseConversion.toBase(id);
+        linkRepository.addLink(new IdDao(id), linkDao);
+        return new ShortLink(link);
+    }
+
+    @Override
+    public LongLink getLink(ShortLink shortLink) throws BaseConversionException {
+        Long id = BaseConversion.fromBase(shortLink.link());
+        if (!linkRepository.checkId(new IdDao(id)))
+            throw new RuntimeException(); //допишу потом
+        LinkDao linkDao = linkRepository.getLink(new IdDao(id));
+        return new LongLink(linkDao.link());
+    }
+
+}
