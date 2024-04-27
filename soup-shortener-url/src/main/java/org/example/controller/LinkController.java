b0d1b94fb2a9e10b876dfafb45fc9e63e98205ac
sour-soup <kiziyepi8@gmail.com>
2024-04-27 13:57:34 +0400

    Добавил Redirect

--- soup-shortener-url/src/main/java/org/example/controller/LinkController.java
+++ soup-shortener-url/src/main/java/org/example/controller/LinkController.java
@@ -9,10 +9,12 @@ import org.example.service.model.User;
 import org.example.utils.BaseConversionException;
 import org.example.utils.URLValidator;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("unused")
 @RestController
 public class LinkController {
     private final LinkService linkService;
@@ -21,21 +23,20 @@ public class LinkController {
         this.linkService = linkService;
     }
 
-    @PostMapping(value = "/addLink")
+    @PostMapping(value = "/api/v1/addLink")
     public LinkDto addLink(@RequestParam String login, @RequestBody LinkDto linkDto) throws BaseConversionException, ParseShortLinkException {
         if (!URLValidator.isValid(linkDto.longLink())) throw new ParseShortLinkException("invalid address format");
 
         Link link = linkService.addLink(new User(login), new Link(linkDto.longLink(), linkDto.shortLink()));
         return new LinkDto(link.longLink(), link.shortLink());
     }
-
     @GetMapping(value = "/{shortLink}")
-    public LinkDto getLink(@PathVariable("shortLink") String shortLink) throws BaseConversionException, EntityNotFoundException, ParseShortLinkException {
+    public RedirectView getLink(@PathVariable("shortLink") String shortLink) throws BaseConversionException, EntityNotFoundException, ParseShortLinkException {
         Link link = linkService.getLink(new Link("", shortLink));
-        return new LinkDto(link.longLink(), link.shortLink());
+        return new RedirectView(link.longLink());
     }
 
-    @GetMapping(value = "/getLinks")
+    @GetMapping(value = "/api/v1/getLinks")
     public List<LinkDto> getUserLinks(@RequestParam String login) {
         List<Link> linkList = linkService.getUserLinks(new User(login));
         List<LinkDto> list = new ArrayList<>();
