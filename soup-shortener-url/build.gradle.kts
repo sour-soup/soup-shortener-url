634f1e1f3bbcdf4fd9833b49ce1a9e797e8cc4d3
sour-soup <kiziyepi8@gmail.com>
2024-04-30 13:31:59 +0400

    Добавил тесты на LinkController

--- soup-shortener-url/build.gradle.kts
+++ soup-shortener-url/build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
     runtimeOnly("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-starter-web-services")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
