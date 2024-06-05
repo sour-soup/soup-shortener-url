bab8cc5dcf4cec60c9e585f67b01be64e58975d9
sour-soup <kiziyepi8@gmail.com>
2024-06-05 19:53:16 +0400

    Убрал исключение BaseConversionException

--- soup-shortener-url/src/main/java/org/example/utils/BaseConversion.java
+++ soup-shortener-url/src/main/java/org/example/utils/BaseConversion.java
@@ -2,13 +2,10 @@ package org.example.utils;
 
 public class BaseConversion {
 
-    private static final Long BASE = 62L;
-    private static final String ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final Long MAX_LENGTH = 10L;
+    private static final String ALPHABET = "_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final Long BASE = (long) ALPHABET.length();
 
-    public static String toBase(Long num) throws BaseConversionException {
-        if (num < 0)
-            throw new BaseConversionException("x must not be negative");
+    public static String toBase(Long num) {
         if (num == 0)
             return String.valueOf(ALPHABET.charAt(0));
         long x = num;
@@ -20,15 +17,11 @@ public class BaseConversion {
         return res.reverse().toString();
     }
 
-    public static Long fromBase(String str) throws BaseConversionException {
-        if (str.length() > MAX_LENGTH)
-            throw new BaseConversionException("string is too long");
+    public static Long fromBase(String str) {
         long res = 0;
         for (char c : str.toCharArray()) {
             res *= BASE;
             long cur = ALPHABET.indexOf(c);
-            if (cur == -1L)
-                throw new BaseConversionException("the string contains non-alphanumeric characters");
             res += cur;
         }
         return res;
--- soup-shortener-url/src/main/java/org/example/utils/BaseConversionException.java
+++
@@ -1,7 +0,0 @@
-package org.example.utils;
-
-public class BaseConversionException extends Exception {
-    BaseConversionException(String message) {
-        super(message);
-    }
-}
