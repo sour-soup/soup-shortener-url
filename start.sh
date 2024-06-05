7d872125c27fcb964b3e50090c89b6c46b404075
sour-soup <kiziyepi8@gmail.com>
2024-06-05 19:51:06 +0400

    Обновил скрипт запуска

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd soup-shortener-url || exit
+./gradlew clean
 ./gradlew bootJar
 cd ..
-docker-compose --env-file local.env up
+docker-compose --env-file local.env stop
+docker-compose --env-file local.env up --build -d
