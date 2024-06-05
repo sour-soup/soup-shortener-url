d12223cf7f391c242ff0b11c05baefecc084b66f
sour-soup <kiziyepi8@gmail.com>
2024-06-05 20:22:24 +0400

    Пофиксил проблемы с десериализацией и транзакциями

--- soup-shortener-url/src/main/java/org/example/config/KafkaConsumerConfig.java
+++ soup-shortener-url/src/main/java/org/example/config/KafkaConsumerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
@@ -25,15 +26,21 @@ public class KafkaConsumerConfig {
     @Bean
     public ConsumerFactory<String, VisitLinkMessage> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                bootstrapServers);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                JsonDeserializer.class);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG,
-                GROUP_ID);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
 
-        return new DefaultKafkaConsumerFactory<>(props);
+        return new DefaultKafkaConsumerFactory<>(
+                props,
+                new StringDeserializer(),
+                new JsonDeserializer<>(VisitLinkMessage.class)
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, VisitLinkMessage> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, VisitLinkMessage> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
     }
 }
--- soup-shortener-url/src/main/java/org/example/kafka/VisitLinkListener.java
+++ soup-shortener-url/src/main/java/org/example/kafka/VisitLinkListener.java
@@ -7,13 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class VisitLinkListener {
     @Autowired
     private LinkRepository linkRepository;
 
-    @KafkaListener(topics = "visit-link", groupId = "link-group")
+    @Transactional
+    @KafkaListener(topics = "visit-link", groupId = "link-group", containerFactory = "kafkaListenerContainerFactory")
     public void listener(@Payload VisitLinkMessage visitLinkMessage) {
         LinkEntity linkEntity = linkRepository.getReferenceById(visitLinkMessage.id());
         linkEntity.setVisitCount(linkEntity.getVisitCount() + 1);
