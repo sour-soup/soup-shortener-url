78e500b4975d6b34df2d1bf46aecfed61833f718
sour-soup <kiziyepi8@gmail.com>
2024-06-06 11:43:32 +0400

    Подключил аунтификацию через keycloak

--- backend/src/main/java/org/example/config/OpenAPIConfig.java
+++ backend/src/main/java/org/example/config/OpenAPIConfig.java
@@ -1,8 +1,10 @@
 package org.example.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -14,6 +16,12 @@ import io.swagger.v3.oas.annotations.info.Info;
                         email = "kiziyepi8@gmail.com"
                 )
         )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
 )
 public class OpenAPIConfig {
 }
